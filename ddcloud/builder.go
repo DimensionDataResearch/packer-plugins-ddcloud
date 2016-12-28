08f3b7578c81adf6e07bca00d492d05d73bd82db
tintoy <tintoy@tintoy.io>
2016-12-28 18:19:09 +1100

    Add CloudControl client.

--- ddcloud/builder.go
+++ ddcloud/builder.go
@@ -1,12 +1,14 @@
 package ddcloud
 
 import "github.com/mitchellh/packer/packer"
+import "github.com/DimensionDataResearch/go-dd-cloud-compute/compute"
 
 // BuilderID is the unique Id for the ddcloud builder
 const BuilderID = "dimension-data-research.ddcloud"
 
 // Builder is the Builder plugin for Packer.
 type Builder struct {
+	client *compute.Client
 }
 
 // Prepare the plugin to run.
@@ -21,5 +23,7 @@ func (builder *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) 
 
 // Cancel plugin execution.
 func (builder *Builder) Cancel() {
-	// TODO: Implement cancellation in CloudControl client.
+	if builder.client != nil {
+		builder.client.Cancel()
+	}
 }
