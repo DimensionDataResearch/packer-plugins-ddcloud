bc6a8bab4d4cc208008b226eaaf04de70bd18afe
tintoy <tintoy@tintoy.io>
2016-12-30 14:53:54 +1100

    Remove diagnostics from CI build.

--- builders/customerimage/builder.go
+++ builders/customerimage/builder.go
@@ -70,6 +70,7 @@ func (builder *Builder) Prepare(settings ...interface{}) (warnings []string, err
 			&steps.ResolveNetworkDomain{},
 			&steps.ResolveVLAN{},
 			&steps.ResolveSourceImage{},
+			// TODO: Ensure target image doesn't already exist.
 			&steps.DeployServer{},
 			&steps.CreateNATRule{},
 			&steps.CreateFirewallRule{},
